5b3717ab4bf1fc3d702a53cd4c67b82ee52505e8
James <hayd0045@algonquinlive.com>
2023-09-20 11:59:44 -0400

    Completes Lab 2

---
+++ lab-2/script.js
@@ -0,0 +1,5 @@
+const firstName = 'James'
+console.log('My name is ' + firstName)
+const favNumber = '17'
+const squared = favNumber*favNumber
+console.log(`The squared of ${favNumber} is ${squared}`)
