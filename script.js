4f2ad0ba3a71817d3269c32586bd7c55f86d561d
Iforgoturbirthday <lisaharding1966@gmail.com>
2023-10-05 22:56:04 -0400

    Completed Lab 4

--- script.js
+++ script.js
@@ -1,68 +1,23 @@
-const textgame = {
-    wood: 0,
-    marshmellows: 3,
-    fire: false,
-    tent: false,
+function setTitle(s){
+    document.getElementById("pageTitle").textContent = s;
+    console.log(`The page title has been updated to ${s} sucessfully.`);
 }
 
-function help(){
-    console.log("Commands: pitch(), search(), tend(), roast(), sleep(), help()")
+function setBackgroundColor(s){
+    document.body.style.backgroundColor = s;
+    console.log(`The background color has been changed to ${s} sucessfully.`);
 }
 
-function pitch(campingGame = textgame){
-    if(campingGame.tent){
-       campingGame.tent = false
-       console.log("'You unpitch your tent.'")
-    } else{
-        campingGame.tent = true
-       console.log("'You pitched the tent.'")
-    }
+function setFontColor(s){
+    document.body.style.color = s;
+    console.log(`The font color has been changed to ${s} sucessfully.`);
 }
 
-function search(campingGame = textgame){
-    if(!campingGame.fire){
-        campingGame.wood += 1
-        console.log("'You found a peice of wood.'")
-    }
-
-}
-
-function tend(campingGame = textgame){
-    if(!campingGame.fire && campingGame.wood >0){
-        campingGame.fire = true
-        console.log("'You started a fire.'")
-    } else if(campingGame.fire){
-        campingGame.fire = false
-        console.log("'You stopped the fire.'")
-    } else{
-        console.log("'You need wood to start a fire.'")
-    }
-
-}
-
-function roast(campingGame = textgame){
-    if(campingGame.fire && campingGame.marshmellows >0){
-        campingGame.marshmellows -= 1
-        console.log("'You roasted a marshmellow.'")
-    } else if(!campingGame.fire){
-        console.log("'You need to start a fire first.'")
-    } else{
-        console.log("'You need a marshmellow.'")
-    }
-}
-
-function sleep(campingGame = textgame){
-    if(!campingGame.fire && campingGame.tent){
-        campingGame.wood = 0
-        campingGame.marshmellows = 3
-        campingGame.fire = false
-        campingGame.tent = false
-        console.log("'You went to sleep.'")
-    } else if(campingGame.fire && campingGame.tent){
-        console.log("'You need to put out the fire.'")
-    } else if(!campingGame.tent && !campingGame.fire){
-        console.log("'You need to pitch the tent.'")
-    } else{
-        console.log("'You need to put out the fire and pitch the tent.'")
+function setTheme(s){
+    document.body.className = s;
+    if (s !== 'theme1' && s !== 'theme2' && s !== 'theme3') {
+        console.log(`Not a valid theme`)
+    } else {
+        console.log(`The theme has been changed to ${s} sucessfully.`) 
     }
 }
